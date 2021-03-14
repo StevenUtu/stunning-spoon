2f88708d41f9d2050d1b7b4c0b22c3c359c07ec3
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-14 16:12:14 +0200

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name[19];
+    printf("What's your name?\n");
+    fgets(name, 16 , stdin);
+    printf("Hello, %s", name);
+    return 0;
+}
+
+// clang -o hello hello.c
+// ./hello
+// mv hello.c hello2.c
