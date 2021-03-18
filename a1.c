8706cd84a38bddfe9b11e4747d21d5779c1de073
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-18 19:10:18 +0200

    Create a1.c

---
+++ a1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+    char s[20];
+    printf(" : ");
+    fgets(s, 16 , stdin);
+    printf(" : ");
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] >= 97 && s[i] <= 122)
+        {
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+      printf("\n");
+}
+
