5f9e0ab769370cf7e5d3952ee2752bc2f6fed77c
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-18 19:58:04 +0200

    Update a1.c
    C

--- a1.c
+++ a1.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+void letters (void);
 int main(void)
 
+{
+    printf("Uppercasing\n");
+    letters();
+}
+
+
+
+void letters (void)
 {
 
     char s[20];
     printf(" : ");
-    fgets(s, 16 , stdin);
+    fgets(s, 16, stdin);
     printf(" : ");
 
     for (int i = 0; s[i] != '\0'; i++)
