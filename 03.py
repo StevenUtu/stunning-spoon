c1ffa2ae7f261fda62fb82eb9dffaa363f554f3e
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-20 23:01:26 +0200

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,20 @@
+while True:
+    i = input('Do you have a problem in your life? (yes/no): ' )
+    x = "Don't worry"
+    yes = 'yes'
+    no = 'no'
+
+    if i.lower().strip() == yes:
+
+        i = input('Can you do someting about it? (yes/no): ').lower().strip()
+        if i == yes:
+            print('{}'.format(x))
+
+        if i == no:
+            print('{}'.format(x))
+            break
+        else:
+            break
+    else:
+        print('{}'.format(x))
+        break
