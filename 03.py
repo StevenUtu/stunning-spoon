235acdf9f18144844a1846bfc8907c5507b1fcf8
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-21 00:04:58 +0200

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,20 +1,35 @@
-while True:
-    i = input('Do you have a problem in your life? (yes/no): ' )
-    x = "Don't worry"
-    yes = 'yes'
-    no = 'no'
+i = input('Do you have a problem in your life? (yes/no) : ')
+x = "Don't worry"
+yes = 'yes'
+no = 'no'
 
-    if i.lower().strip() == yes:
+if i.lower().strip() == yes:
 
-        i = input('Can you do someting about it? (yes/no): ').lower().strip()
-        if i == yes:
-            print('{}'.format(x))
-
-        if i == no:
-            print('{}'.format(x))
-            break
-        else:
-            break
-    else:
+    i = input('Can you do something about it ? (yes/no): ').lower().strip()
+    if i == yes:
         print('{}'.format(x))
-        break
+    if i == no:
+        print('{}'.format(x))
+else:
+    print('{}'.format(x))
+
+# while True:
+#     i = input('Do you have a problem in your life? (yes/no): ' )
+#     x = "Don't worry"
+#     yes = 'yes'
+#     no = 'no'
+
+#     if i.lower().strip() == yes:
+
+#         i = input('Can you do someting about it? (yes/no): ').lower().strip()
+#         if i == yes:
+#             print('{}'.format(x))
+
+#         if i == no:
+#             print('{}'.format(x))
+#             break
+#         else:
+#             break
+#     else:
+#         print('{}'.format(x))
+#         break
