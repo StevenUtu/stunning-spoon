4b83adad1bd24460e83b00504be14cd20491cbad
wordsguy <74150238+wordsguy@users.noreply.github.com>
2021-03-22 16:12:28 +0200

    Update 04.py

--- 04.py
+++ 04.py
@@ -12,13 +12,18 @@ class characters:
     def characters_data(self):
         print('{} {} {} {}'.format(self.title, self.character_name, self.power_score, self.biography))
 
+    def characters_vm(self):
+        print('{} {}\n {}'.format(self.title, self.character_name, self.biography))
+
 
 B_Cider = characters('Barbarian', 'Cider', '4854', 'Not much is known about this character')
 L_Cido = characters('Lord', 'Cido', '7910', 'Not much is known about this character' )
 N_Grandmaster = characters('New', 'Grandmaster', '999999999999999', 'The more i know about the past the futher I can see in future' )
+T_Universe = characters('The', 'Universe', '999999999999999', 'Space and time and their contents, including planets, stars, galaxies, and all other forms of matter and energy.')
 
 # Y(Height) of ascii-art
 z = 12
+print("\n-Aventure Game-\n")
 for x in range(z):
     print("-" * (z-x) + "*" * x + " " * x + "-" * (z-x))
 # ...............
@@ -33,6 +38,7 @@ noAnswers = {'no', 'n', 'nope', 'nah', 'not'}
 Answers_Attack = {'attack', 'att', 'a', 'ack', 'atck', 'atk', 'tt', 'k'}
 Answers_Talk = {'talk', 't', 'a', 'tak', 'speak', 'hello', 'hi', 'hey'}
 Answers_Who = {'who', 'are', 'who are you', 'what', 'do I know you?', 'who are you?', '?', 'hey'}
+Answers_Space = {'space', 'universe', 'life', 'galaxy', 'what is it?', 'where?', 'why', 'new'}
 
 if answer in yesAnswers:
     answer = input(first_selection).lower().strip()
@@ -41,16 +47,28 @@ if answer in yesAnswers:
         print('____'.format(characters.characters_data(B_Cider)))
         answer = input('\nYou have encountered a Friendly enemy\n (Attack/Talk) :').lower().strip()
         if answer in Answers_Attack:
-            print('-4858 damage')
+            print('-999999999 damage')
             print('Game Over')
         elif answer in Answers_Talk:
             print('Greetings,')
-            print('So what would you like talk about?'.format(characters.title_name(B_Cider)))
+            print('So about what would you like talk?'.format(characters.title_name(B_Cider)))
             answer = input('\n____\n:').lower().strip()
         if answer in Answers_Who:
             print("I'm just a traveler".format(characters.characters_data(N_Grandmaster)))
+        elif answer in Answers_Space:
+            print(''.format(characters.characters_vm(T_Universe)))
     if answer in noAnswers:
         print(not_Ahero)
         print('____'.format(characters.characters_data(L_Cido)))
+        answer = input('\nYou have encountered a Friend\n (Attack/Talk):' ).lower().strip()
+        if answer in Answers_Attack:
+            print('-7910 damage')
+            print("Game Over")
+        elif answer in Answers_Talk:
+            print("How's it going?")
+            print(''.format(characters.title_name(B_Cider)))
+            answer = input('\n:').lower().strip()
+        if answer in Answers_Space:
+            print(''.format(characters.characters_vm(T_Universe)))
 else:
     print(error)
